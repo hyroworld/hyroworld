5d961c0dbe57f7afd5eb15b4455ce72a275080fe
장혜승 <hyesech@gmail.com>
2022-01-26 17:05:31 +0900

    chore: global style 및 구조 작성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,53 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import styled from "@emotion/styled";
+import { Code } from ".";
+
+const Container = styled.div`
+  width: 100%;
+`;
+const Header = styled.header`
+  position: sticky;
+  top: 0;
+  height: 60px;
+  max-height: 60px;
+`;
+const Main = styled.main`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Footer = styled.footer`
+  display: flex;
+  padding: 2rem 0;
+  border-top: 1px solid #eaeaea;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Tag = styled.span`
+  font-size: 2rem;
+  font-weight: 800;
+  background-color: #000000;
+  color: #ffffff;
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Tag>klldcll</Tag>
+      </Header>
+      <Main>
+        <Component {...pageProps} />
+      </Main>
+      <Footer>
+        <Code>footer</Code>
+      </Footer>
+    </Container>
+  );
 }
 
 export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+
+class MainDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+
+          {/* Below: portal wrapper */}
+          <div id="portal" />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MainDocument;
